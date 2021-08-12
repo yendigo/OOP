514e604a5c40412bf58e1ef7ce9f6e1427d26db1
Yen Sangalang <yengestrudelsangalang@iskolarngbayan.pup.edu.ph>
2021-08-12 14:44:40 +0800

    Add files via upload

---
+++ Pizza/Pizza/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿
+namespace Pizza
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radPrimo = new System.Windows.Forms.RadioButton();
+            this.radSpecial = new System.Windows.Forms.RadioButton();
+            this.radDeluxe = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chkSalami = new System.Windows.Forms.CheckBox();
+            this.chkTomato = new System.Windows.Forms.CheckBox();
+            this.chkOnions = new System.Windows.Forms.CheckBox();
+            this.chkMushroom = new System.Windows.Forms.CheckBox();
+            this.chkBacon = new System.Windows.Forms.CheckBox();
+            this.chkPepper = new System.Windows.Forms.CheckBox();
+            this.chkCheese = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.radPrimo);
+            this.groupBox1.Controls.Add(this.radSpecial);
+            this.groupBox1.Controls.Add(this.radDeluxe);
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(26, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(172, 148);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pizza";
+            // 
+            // radPrimo
+            // 
+            this.radPrimo.AutoSize = true;
+            this.radPrimo.BackColor = System.Drawing.Color.Transparent;
+            this.radPrimo.Location = new System.Drawing.Point(28, 95);
+            this.radPrimo.Name = "radPrimo";
+            this.radPrimo.Size = new System.Drawing.Size(73, 23);
+            this.radPrimo.TabIndex = 2;
+            this.radPrimo.TabStop = true;
+            this.radPrimo.Text = "Primo";
+            this.radPrimo.UseVisualStyleBackColor = false;
+            this.radPrimo.CheckedChanged += new System.EventHandler(this.radPrimo_CheckedChanged);
+            // 
+            // radSpecial
+            // 
+            this.radSpecial.AutoSize = true;
+            this.radSpecial.BackColor = System.Drawing.Color.Transparent;
+            this.radSpecial.Location = new System.Drawing.Point(28, 66);
+            this.radSpecial.Name = "radSpecial";
+            this.radSpecial.Size = new System.Drawing.Size(84, 23);
+            this.radSpecial.TabIndex = 1;
+            this.radSpecial.TabStop = true;
+            this.radSpecial.Text = "Special";
+            this.radSpecial.UseVisualStyleBackColor = false;
+            this.radSpecial.CheckedChanged += new System.EventHandler(this.radSpecial_CheckedChanged);
+            // 
+            // radDeluxe
+            // 
+            this.radDeluxe.AutoSize = true;
+            this.radDeluxe.BackColor = System.Drawing.Color.Transparent;
+            this.radDeluxe.Location = new System.Drawing.Point(28, 37);
+            this.radDeluxe.Name = "radDeluxe";
+            this.radDeluxe.Size = new System.Drawing.Size(79, 23);
+            this.radDeluxe.TabIndex = 0;
+            this.radDeluxe.TabStop = true;
+            this.radDeluxe.Text = "Deluxe";
+            this.radDeluxe.UseVisualStyleBackColor = false;
+            this.radDeluxe.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(26, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Price :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.chkSalami);
+            this.groupBox2.Controls.Add(this.chkTomato);
+            this.groupBox2.Controls.Add(this.chkOnions);
+            this.groupBox2.Controls.Add(this.chkMushroom);
+            this.groupBox2.Controls.Add(this.chkBacon);
+            this.groupBox2.Controls.Add(this.chkPepper);
+            this.groupBox2.Controls.Add(this.chkCheese);
+            this.groupBox2.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(227, 35);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(287, 255);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ingredients";
+            // 
+            // chkSalami
+            // 
+            this.chkSalami.AutoSize = true;
+            this.chkSalami.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.chkSalami.Location = new System.Drawing.Point(28, 212);
+            this.chkSalami.Name = "chkSalami";
+            this.chkSalami.Size = new System.Drawing.Size(80, 23);
+            this.chkSalami.TabIndex = 5;
+            this.chkSalami.Text = "Salami";
+            this.chkSalami.UseVisualStyleBackColor = true;
+            // 
+            // chkTomato
+            // 
+            this.chkTomato.AutoSize = true;
+            this.chkTomato.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.chkTomato.Location = new System.Drawing.Point(28, 183);
+            this.chkTomato.Name = "chkTomato";
+            this.chkTomato.Size = new System.Drawing.Size(191, 23);
+            this.chkTomato.TabIndex = 2;
+            this.chkTomato.Text = "Tomato and Pineapple";
+            this.chkTomato.UseVisualStyleBackColor = true;
+            // 
+            // chkOnions
+            // 
+            this.chkOnions.AutoSize = true;
+            this.chkOnions.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.chkOnions.Location = new System.Drawing.Point(28, 154);
+            this.chkOnions.Name = "chkOnions";
+            this.chkOnions.Size = new System.Drawing.Size(151, 23);
+            this.chkOnions.TabIndex = 4;
+            this.chkOnions.Text = "Onions and Chili";
+            this.chkOnions.UseVisualStyleBackColor = true;
+            // 
+            // chkMushroom
+            // 
+            this.chkMushroom.AutoSize = true;
+            this.chkMushroom.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.chkMushroom.Location = new System.Drawing.Point(28, 125);
+            this.chkMushroom.Name = "chkMushroom";
+            this.chkMushroom.Size = new System.Drawing.Size(107, 23);
+            this.chkMushroom.TabIndex = 3;
+            this.chkMushroom.Text = "Mushroom";
+            this.chkMushroom.UseVisualStyleBackColor = true;
+            // 
+            // chkBacon
+            // 
+            this.chkBacon.AutoSize = true;
+            this.chkBacon.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.chkBacon.Location = new System.Drawing.Point(28, 96);
+            this.chkBacon.Name = "chkBacon";
+            this.chkBacon.Size = new System.Drawing.Size(148, 23);
+            this.chkBacon.TabIndex = 2;
+            this.chkBacon.Text = "Bacon and Ham";
+            this.chkBacon.UseVisualStyleBackColor = true;
+            // 
+            // chkPepper
+            // 
+            this.chkPepper.AutoSize = true;
+            this.chkPepper.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.chkPepper.Location = new System.Drawing.Point(28, 67);
+            this.chkPepper.Name = "chkPepper";
+            this.chkPepper.Size = new System.Drawing.Size(84, 23);
+            this.chkPepper.TabIndex = 1;
+            this.chkPepper.Text = "Pepper";
+            this.chkPepper.UseVisualStyleBackColor = true;
+            // 
+            // chkCheese
+            // 
+            this.chkCheese.AutoSize = true;
+            this.chkCheese.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.chkCheese.Location = new System.Drawing.Point(28, 37);
+            this.chkCheese.Name = "chkCheese";
+            this.chkCheese.Size = new System.Drawing.Size(87, 23);
+            this.chkCheese.TabIndex = 0;
+            this.chkCheese.Text = "Cheese";
+            this.chkCheese.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(26, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Quantity :";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(111, 221);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.ReadOnly = true;
+            this.txtPrice.Size = new System.Drawing.Size(87, 27);
+            this.txtPrice.TabIndex = 6;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(111, 255);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(87, 27);
+            this.txtQuantity.TabIndex = 7;
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCompute.Location = new System.Drawing.Point(170, 336);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(206, 29);
+            this.btnCompute.TabIndex = 8;
+            this.btnCompute.Text = "Compute Now!";
+            this.btnCompute.UseVisualStyleBackColor = false;
+            this.btnCompute.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(25, 400);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 19);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Total Amount :";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(141, 396);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(125, 27);
+            this.txtAmount.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(542, 465);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCompute);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Pizza";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radPrimo;
+        private System.Windows.Forms.RadioButton radSpecial;
+        private System.Windows.Forms.RadioButton radDeluxe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chkSalami;
+        private System.Windows.Forms.CheckBox chkTomato;
+        private System.Windows.Forms.CheckBox chkOnions;
+        private System.Windows.Forms.CheckBox chkMushroom;
+        private System.Windows.Forms.CheckBox chkBacon;
+        private System.Windows.Forms.CheckBox chkPepper;
+        private System.Windows.Forms.CheckBox chkCheese;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Button btnCompute;
+    }
+}
+
---
+++ Pizza/Pizza/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pizza
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radDeluxe.Checked)
+            {
+                chkCheese.Checked = true;
+                chkBacon.Checked = true;
+                chkOnions.Checked = true;
+                txtPrice.Text = "Php 185.00";
+            }
+        }
+
+        private void radSpecial_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radSpecial.Checked)
+            {
+                chkCheese.Checked = true;
+                chkPepper.Checked = true;
+                chkBacon.Checked = true;
+                chkMushroom.Checked = true;
+                chkOnions.Checked = true;
+                txtPrice.Text = "Php 275.00";
+            }
+        }
+
+        private void radPrimo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radPrimo.Checked)
+            {
+                chkCheese.Checked = true;
+                chkPepper.Checked = true;
+                chkBacon.Checked = true;
+                chkMushroom.Checked = true;
+                chkOnions.Checked = true;
+                chkTomato.Checked = true;
+                chkSalami.Checked = true;
+                txtPrice.Text = "Php 350.00";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(txtQuantity.Text);
+
+            if (radDeluxe.Checked)
+            {
+                int result1 = 185 * x;
+                txtAmount.Text = result1.ToString();
+            }
+            else if (radSpecial.Checked)
+            {
+                int result2 = 275 * x;
+                txtAmount.Text = result2.ToString();
+            }
+            else if (radPrimo.Checked)
+            {
+                int result3 = 350 * x;
+                txtAmount.Text = result3.ToString();
+            }
+        }
+    }
+}
